69a3f10d1cf2eaaa699e466281436eb132b1eed1
RelaxStation <151868654+RelaxStation@users.noreply.github.com>
2024-10-04 23:20:34 +0330

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -201,7 +201,7 @@ IP=$(ip route get 1 | sed -n 's/^.*src \([0-9.]*\) .*$/\1/p')
 current_time=$(date +"%d/%m/%Y, %I:%M:%S %p")
 caption="${caption}\n${Notes}\nForked By Boofi Team\nScheduled Backup Created At: ${current_time}"
 comment=$(echo -e "$caption" | sed 's/<code>//g;s/<\/code>//g')
-comment=$(trim "$comment")
+comment=\$(echo -n "\$comment" | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
 
 # install zip
 # نصب پکیج zip
