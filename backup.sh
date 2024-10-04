ca32475da0db436afabfcedd8c91b664141305e5
RelaxStation <151868654+RelaxStation@users.noreply.github.com>
2024-10-05 01:57:05 +0330

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Bot token
-# گرفتن توکن ربات از کاربر و ذخیره آن در متغیر tk
+# Prompting the user for the bot token and storing it in variable tk
 while [[ -z "$tk" ]]; do
     echo "Bot token: "
     read -r tk
@@ -12,7 +12,7 @@ while [[ -z "$tk" ]]; do
 done
 
 # Chat id
-# گرفتن Chat ID از کاربر و ذخیره آن در متغیر chatid
+# Prompting the user for the Chat ID and storing it in variable chatid
 while [[ -z "$chatid" ]]; do
     echo "Chat id: "
     read -r chatid
@@ -26,12 +26,12 @@ while [[ -z "$chatid" ]]; do
 done
 
 # Caption
-# گرفتن عنوان برای فایل پشتیبان و ذخیره آن در متغیر caption
+# Prompting the user for a caption for the backup file and storing it in variable caption
 echo "Caption (for example, your domain, to identify the database file more easily): "
 read -r caption
 
 # Cronjob
-# تعیین زمانی برای اجرای این اسکریپت به صورت دوره‌ای
+# Setting up a schedule to run this script periodically
 while true; do
     echo "Cronjob (minutes and hours) (e.g : 30 6 or 0 12) : "
     read -r minute hour
@@ -52,9 +52,8 @@ while true; do
     fi
 done
 
-
 # x-ui or marzban or hiddify
-# گرفتن نوع نرم افزاری که می‌خواهیم پشتیبانی از آن بگیریم و ذخیره آن در متغیر xmh
+# Prompting the user for the type of software to backup and storing it in variable xmh
 while [[ -z "$xmh" ]]; do
     echo "x-ui or marzban or hiddify? [x/m/h] : "
     read -r xmh
@@ -80,50 +79,47 @@ while [[ -z "$crontabs" ]]; do
 done
 
 if [[ "$crontabs" == "y" ]]; then
-# remove cronjobs
-sudo crontab -l | grep -vE '/root/PanelBackup.+\.sh' | crontab -
+    # Remove existing cron jobs related to PanelBackup
+    sudo crontab -l | grep -vE '/root/PanelBackup.+\.sh' | crontab -
 fi
-
 
 # m backup
-# ساخت فایل پشتیبانی برای نرم‌افزار Marzban و ذخیره آن در فایل PanelBackup.zip
+# Creating a backup file for Marzban software and storing it in PanelBackup.zip
 if [[ "$xmh" == "m" ]]; then
 
-if dir=$(find /opt /root -type d -iname "marzban" -print -quit); then
-  echo "The folder exists at $dir"
-else
-  echo "The folder does not exist."
-  exit 1
-fi
+    if dir=$(find /opt /root -type d -iname "marzban" -print -quit); then
+        echo "The folder exists at $dir"
+    else
+        echo "The folder does not exist."
+        exit 1
+    fi
 
-if [ -d "/var/lib/marzban/mysql" ]; then
+    if [ -d "/var/lib/marzban/mysql" ]; then
 
-  sed -i -e 's/\s*=\s*/=/' -e 's/\s*:\s*/:/' -e 's/^\s*//' /opt/marzban/.env
+        sed -i -e 's/\s*=\s*/=/' -e 's/\s*:\s*/:/' -e 's/^\s*//' /opt/marzban/.env
 
-  docker exec marzban-mysql-1 bash -c "mkdir -p /var/lib/mysql/db-backup"
-  source /opt/marzban/.env
+        docker exec marzban-mysql-1 bash -c "mkdir -p /var/lib/mysql/db-backup"
+        source /opt/marzban/.env
 
-    cat > "/var/lib/marzban/mysql/PanelBackup.sh" <<EOL
+        cat > "/var/lib/marzban/mysql/PanelBackup.sh" <<EOL
 #!/bin/bash
 
 USER="root"
 PASSWORD="$MYSQL_ROOT_PASSWORD"
-
 
 databases=\$(mysql --user=\$USER --password=\$PASSWORD -e "SHOW DATABASES;" | tr -d "| " | grep -v Database)
 
 for db in \$databases; do
     if [[ "\$db" != "information_schema" ]] && [[ "\$db" != "mysql" ]] && [[ "\$db" != "performance_schema" ]] && [[ "\$db" != "sys" ]] ; then
         echo "Dumping database: \$db"
-		mysqldump --force --opt --user=\$USER --password=\$PASSWORD --databases \$db > /var/lib/mysql/db-backup/\$db.sql
-
+        mysqldump --force --opt --user=\$USER --password=\$PASSWORD --databases \$db > /var/lib/mysql/db-backup/\$db.sql
     fi
 done
 
 EOL
-chmod +x /var/lib/marzban/mysql/PanelBackup.sh
+        chmod +x /var/lib/marzban/mysql/PanelBackup.sh
 
-ZIP=$(cat <<EOF
+        ZIP=$(cat <<EOF
 docker exec marzban-mysql-1 bash -c "/var/lib/mysql/PanelBackup.sh"
 zip -r /root/PanelBackup-m.zip /opt/marzban/* /var/lib/marzban/* /opt/marzban/.env -x /var/lib/marzban/mysql/\*
 zip -r /root/PanelBackup-m.zip /var/lib/marzban/mysql/db-backup/*
@@ -132,44 +128,44 @@ EOF
 )
 
     else
-      ZIP="zip -r /root/PanelBackup-m.zip ${dir}/* /var/lib/marzban/* /opt/marzban/.env"
-fi
+        ZIP="zip -r /root/PanelBackup-m.zip ${dir}/* /var/lib/marzban/* /opt/marzban/.env"
+    fi
 
-Notes="Marzban Backup"
+    Notes="Marzban Backup"
 
 # x-ui backup
-# ساخت فایل پشتیبانی برای نرم‌افزار X-UI و ذخیره آن در فایل PanelBackup.zip
+# Creating a backup file for X-UI software and storing it in PanelBackup.zip
 elif [[ "$xmh" == "x" ]]; then
 
-if dbDir=$(find /etc -type d -iname "x-ui*" -print -quit); then
-  echo "The folder exists at $dbDir"
-else
-  echo "The folder does not exist."
-  exit 1
-fi
+    if dbDir=$(find /etc -type d -iname "x-ui*" -print -quit); then
+        echo "The folder exists at $dbDir"
+    else
+        echo "The folder does not exist."
+        exit 1
+    fi
 
-if configDir=$(find /usr/local -type d -iname "x-ui*" -print -quit); then
-  echo "The folder exists at $configDir"
-else
-  echo "The folder does not exist."
-  exit 1
-fi
+    if configDir=$(find /usr/local -type d -iname "x-ui*" -print -quit); then
+        echo "The folder exists at $configDir"
+    else
+        echo "The folder does not exist."
+        exit 1
+    fi
 
-ZIP="zip /root/PanelBackup-x.zip ${dbDir}/x-ui.db ${configDir}/config.json"
-Notes="x-ui backup"
+    ZIP="zip /root/PanelBackup-x.zip ${dbDir}/x-ui.db ${configDir}/config.json"
+    Notes="x-ui backup"
 
 # hiddify backup
-# ساخت فایل پشتیبانی برای نرم‌افزار Hiddify و ذخیره آن در فایل PanelBackup.zip
+# Creating a backup file for Hiddify software and storing it in PanelBackup.zip
 elif [[ "$xmh" == "h" ]]; then
 
-if ! find /opt/hiddify-config/hiddify-panel/ -type d -iname "backup" -print -quit; then
-  echo "The folder does not exist."
-  exit 1
-fi
+    if ! find /opt/hiddify-config/hiddify-panel/ -type d -iname "backup" -print -quit; then
+        echo "The folder does not exist."
+        exit 1
+    fi
 
-ZIP=$(cat <<EOF
+    ZIP=$(cat <<EOF
 cd /opt/hiddify-config/hiddify-panel/
-if [ $(find /opt/hiddify-config/hiddify-panel/backup -type f | wc -l) -gt 100 ]; then
+if [ \$(find /opt/hiddify-config/hiddify-panel/backup -type f | wc -l) -gt 100 ]; then
   find /opt/hiddify-config/hiddify-panel/backup -type f -delete
 fi
 python3 -m hiddifypanel backup
@@ -180,13 +176,13 @@ zip /root/PanelBackup-h.zip /opt/hiddify-config/hiddify-panel/backup/\$latest_fi
 
 EOF
 )
-Notes="hiddify backup"
+    Notes="hiddify backup"
 else
-echo "Please choose m or x or h only !"
-exit 1
+    echo "Please choose m or x or h only!"
+    exit 1
 fi
 
-
+# Function to trim whitespace
 trim() {
     # remove leading and trailing whitespace/lines
     local var="$*"
@@ -197,33 +193,41 @@ trim() {
     echo -n "$var"
 }
 
-IP=$(ip route get 1 | sed -n 's/^.*src \([0-9.]*\) .*$/\1/p')
-caption="${caption}\n${Notes}\nForked By Boofi Team\nScheduled Backup Created At: $(date +"%d/%m/%Y, %I:%M:%S %p")"
-comment=$(echo -e "$caption" | sed 's/<code>//g;s/<\/code>//g')
-comment=$(echo -n "$comment" | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
-
-# install zip
-# نصب پکیج zip
+# Install zip if not already installed
 sudo apt install zip -y
 
-# send backup to telegram
-# ارسال فایل پشتیبانی به تلگرام
+# Create the script that will be executed by cron
 cat > "/root/PanelBackup-${xmh}.sh" <<EOL
+#!/bin/bash
+
+# Calculate current time inside the script
+current_time=\$(date +"%d/%m/%Y, %I:%M:%S %p")
+
+# Reconstruct the caption with updated time
+caption="${caption}"
+Notes="${Notes}"
+caption="\${caption}\n\${Notes}\nForked By Boofi Team\nScheduled Backup Created At: \${current_time}"
+
+comment=\$(echo -e "\$caption" | sed 's/<code>//g;s/<\/code>//g')
+comment=\$(echo -n "\$comment" | sed 's/^[[:space:]]*//;s/[[:space:]]*\$//')
+
+# Perform the backup
 rm -rf /root/PanelBackup-${xmh}.zip
 $ZIP
-echo -e "$comment" | zip -z /root/PanelBackup-${xmh}.zip
-curl -F chat_id="${chatid}" -F caption="$comment" -F parse_mode="HTML" -F document=@"/root/PanelBackup-${xmh}.zip" https://api.telegram.org/bot${tk}/sendDocument
+echo -e "\$comment" | zip -z /root/PanelBackup-${xmh}.zip
+
+# Send the backup to Telegram
+curl -F chat_id="${chatid}" -F caption="\$comment" -F parse_mode="HTML" -F document=@"/root/PanelBackup-${xmh}.zip" https://api.telegram.org/bot${tk}/sendDocument
 EOL
 
+# Make the script executable
+chmod +x "/root/PanelBackup-${xmh}.sh"
 
-# Add cronjob
-# افزودن کرانجاب جدید برای اجرای دوره‌ای این اسکریپت
+# Add the cron job to execute the script periodically
 { crontab -l -u root; echo "${cron_time} /bin/bash /root/PanelBackup-${xmh}.sh >/dev/null 2>&1"; } | crontab -u root -
 
-# run the script
-# اجرای این اسکریپت
+# Run the script immediately
 bash "/root/PanelBackup-${xmh}.sh"
 
-# Done
-# پایان اجرای اسکریپت
+# Completion message
 echo -e "\nDone\n"
