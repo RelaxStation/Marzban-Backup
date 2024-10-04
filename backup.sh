f10f233d250f4d340cc0400e57fd1e65c637bc64
RelaxStation <151868654+RelaxStation@users.noreply.github.com>
2024-10-05 01:17:14 +0330

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -52,6 +52,7 @@ while true; do
     fi
 done
 
+
 # x-ui or marzban or hiddify
 # گرفتن نوع نرم افزاری که می‌خواهیم پشتیبانی از آن بگیریم و ذخیره آن در متغیر xmh
 while [[ -z "$xmh" ]]; do
@@ -79,84 +80,96 @@ while [[ -z "$crontabs" ]]; do
 done
 
 if [[ "$crontabs" == "y" ]]; then
-    # remove cronjobs
-    sudo crontab -l | grep -vE '/root/PanelBackup.+\.sh' | crontab -
+# remove cronjobs
+sudo crontab -l | grep -vE '/root/PanelBackup.+\.sh' | crontab -
 fi
 
+
 # m backup
+# ساخت فایل پشتیبانی برای نرم‌افزار Marzban و ذخیره آن در فایل PanelBackup.zip
 if [[ "$xmh" == "m" ]]; then
 
-    if dir=$(find /opt /root -type d -iname "marzban" -print -quit); then
-      echo "The folder exists at $dir"
-    else
-      echo "The folder does not exist."
-      exit 1
-    fi
+if dir=$(find /opt /root -type d -iname "marzban" -print -quit); then
+  echo "The folder exists at $dir"
+else
+  echo "The folder does not exist."
+  exit 1
+fi
 
-    if [ -d "/var/lib/marzban/mysql" ]; then
-      sed -i -e 's/\s*=\s*/=/' -e 's/\s*:\s*/:/' -e 's/^\s*//' /opt/marzban/.env
-      docker exec marzban-mysql-1 bash -c "mkdir -p /var/lib/mysql/db-backup"
-      source /opt/marzban/.env
+if [ -d "/var/lib/marzban/mysql" ]; then
 
-      cat > "/var/lib/marzban/mysql/PanelBackup.sh" <<EOL
+  sed -i -e 's/\s*=\s*/=/' -e 's/\s*:\s*/:/' -e 's/^\s*//' /opt/marzban/.env
+
+  docker exec marzban-mysql-1 bash -c "mkdir -p /var/lib/mysql/db-backup"
+  source /opt/marzban/.env
+
+    cat > "/var/lib/marzban/mysql/PanelBackup.sh" <<EOL
 #!/bin/bash
 
 USER="root"
 PASSWORD="$MYSQL_ROOT_PASSWORD"
+
 
 databases=\$(mysql --user=\$USER --password=\$PASSWORD -e "SHOW DATABASES;" | tr -d "| " | grep -v Database)
 
 for db in \$databases; do
     if [[ "\$db" != "information_schema" ]] && [[ "\$db" != "mysql" ]] && [[ "\$db" != "performance_schema" ]] && [[ "\$db" != "sys" ]] ; then
         echo "Dumping database: \$db"
-        mysqldump --force --opt --user=\$USER --password=\$PASSWORD --databases \$db > /var/lib/mysql/db-backup/\$db.sql
+		mysqldump --force --opt --user=\$USER --password=\$PASSWORD --databases \$db > /var/lib/mysql/db-backup/\$db.sql
+
     fi
 done
+
 EOL
+chmod +x /var/lib/marzban/mysql/PanelBackup.sh
 
-      chmod +x /var/lib/marzban/mysql/PanelBackup.sh
-
-      ZIP=$(cat <<EOF
+ZIP=$(cat <<EOF
 docker exec marzban-mysql-1 bash -c "/var/lib/mysql/PanelBackup.sh"
 zip -r /root/PanelBackup-m.zip /opt/marzban/* /var/lib/marzban/* /opt/marzban/.env -x /var/lib/marzban/mysql/\*
 zip -r /root/PanelBackup-m.zip /var/lib/marzban/mysql/db-backup/*
 rm -rf /var/lib/marzban/mysql/db-backup/*
 EOF
 )
+
     else
       ZIP="zip -r /root/PanelBackup-m.zip ${dir}/* /var/lib/marzban/* /opt/marzban/.env"
-    fi
-    Notes="Marzban Backup"
+fi
+
+Notes="Marzban Backup"
 
 # x-ui backup
+# ساخت فایل پشتیبانی برای نرم‌افزار X-UI و ذخیره آن در فایل PanelBackup.zip
 elif [[ "$xmh" == "x" ]]; then
-    if dbDir=$(find /etc -type d -iname "x-ui*" -print -quit); then
-      echo "The folder exists at $dbDir"
-    else
-      echo "The folder does not exist."
-      exit 1
-    fi
 
-    if configDir=$(find /usr/local -type d -iname "x-ui*" -print -quit); then
-      echo "The folder exists at $configDir"
-    else
-      echo "The folder does not exist."
-      exit 1
-    fi
+if dbDir=$(find /etc -type d -iname "x-ui*" -print -quit); then
+  echo "The folder exists at $dbDir"
+else
+  echo "The folder does not exist."
+  exit 1
+fi
 
-    ZIP="zip /root/PanelBackup-x.zip ${dbDir}/x-ui.db ${configDir}/config.json"
-    Notes="x-ui backup"
+if configDir=$(find /usr/local -type d -iname "x-ui*" -print -quit); then
+  echo "The folder exists at $configDir"
+else
+  echo "The folder does not exist."
+  exit 1
+fi
+
+ZIP="zip /root/PanelBackup-x.zip ${dbDir}/x-ui.db ${configDir}/config.json"
+Notes="x-ui backup"
 
 # hiddify backup
+# ساخت فایل پشتیبانی برای نرم‌افزار Hiddify و ذخیره آن در فایل PanelBackup.zip
 elif [[ "$xmh" == "h" ]]; then
-    if ! find /opt/hiddify-config/hiddify-panel/ -type d -iname "backup" -print -quit; then
-      echo "The folder does not exist."
-      exit 1
-    fi
 
-    ZIP=$(cat <<EOF
+if ! find /opt/hiddify-config/hiddify-panel/ -type d -iname "backup" -print -quit; then
+  echo "The folder does not exist."
+  exit 1
+fi
+
+ZIP=$(cat <<EOF
 cd /opt/hiddify-config/hiddify-panel/
-if [ \$(find /opt/hiddify-config/hiddify-panel/backup -type f | wc -l) -gt 100 ]; then
+if [ $(find /opt/hiddify-config/hiddify-panel/backup -type f | wc -l) -gt 100 ]; then
   find /opt/hiddify-config/hiddify-panel/backup -type f -delete
 fi
 python3 -m hiddifypanel backup
@@ -164,16 +177,27 @@ cd /opt/hiddify-config/hiddify-panel/backup
 latest_file=\$(ls -t *.json | head -n1)
 rm -f /root/PanelBackup-h.zip
 zip /root/PanelBackup-h.zip /opt/hiddify-config/hiddify-panel/backup/\$latest_file
+
 EOF
 )
-    Notes="hiddify backup"
+Notes="hiddify backup"
 else
-    echo "Please choose m or x or h only!"
-    exit 1
+echo "Please choose m or x or h only !"
+exit 1
 fi
 
-# Send backup to telegram
-cat > "/root/PanelBackup-${xmh}.sh" <<'EOL'
+
+trim() {
+    # remove leading and trailing whitespace/lines
+    local var="$*"
+    # remove leading whitespace characters
+    var="${var#"${var%%[![:space:]]*}"}"
+    # remove trailing whitespace characters
+    var="${var%"${var##*[![:space:]]}"}"
+    echo -n "$var"
+}
+
+cat > "/root/PanelBackup-${xmh}.sh" <<EOL
 #!/bin/bash
 
 # Remove old backup
@@ -183,10 +207,23 @@ rm -rf /root/PanelBackup-${xmh}.zip
 $ZIP
 
 # Get the current time and IP
-IP=$(ip route get 1 | sed -n 's/^.*src \([0-9.]*\) .*$/\1/p')
-current_time=$(date +"%d/%m/%Y, %I:%M:%S %p")
+IP=\$(ip route get 1 | sed -n 's/^.*src \([0-9.]*\) .*$/\1/p')
+current_time=\$(date +"%d/%m/%Y, %I:%M:%S %p")
 
 # Prepare the caption dynamically each time
-caption="${caption}\n${Notes}\nForked By Boofi Team\nScheduled Backup Created At: ${current_time}"
-comment
+caption="${caption}\n${Notes}\nForked By Boofi Team\nScheduled Backup Created At: \${current_time}"
+comment=\$(echo -e "\$caption" | sed 's/<code>//g;s/<\/code>//g')
+comment=\$(echo -n "\$comment" | sed 's/^[[:space:]]*//;s/[[:space:]]*\$//')
 EOL
+
+# Add cronjob
+# افزودن کرانجاب جدید برای اجرای دوره‌ای این اسکریپت
+{ crontab -l -u root; echo "${cron_time} /bin/bash /root/PanelBackup-${xmh}.sh >/dev/null 2>&1"; } | crontab -u root -
+
+# run the script
+# اجرای این اسکریپت
+bash "/root/PanelBackup-${xmh}.sh"
+
+# Done
+# پایان اجرای اسکریپت
+echo -e "\nDone\n"
